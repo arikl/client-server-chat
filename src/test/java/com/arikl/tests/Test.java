7c9978c3f17c50e17f3a3511221b46a27b4f7349
arikl <arikl@liveperson.com>
2017-03-15 12:35:46 +0200

    added unit test structure

---
+++ src/test/java/com/arikl/tests/Test.java
@@ -0,0 +1,21 @@
+package com.arikl.tests;
+
+/**
+ * Created by arikl on 3/15/17.
+ */
+public class Test {
+
+    @org.junit.Test
+    public void testClient(){
+        //todo: check client get messages
+
+    }
+
+    @org.junit.Test
+    public void testServer(){
+        //todo: check server get messages
+
+    }
+
+
+}
